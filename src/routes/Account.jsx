7221be4992872937a98b8e7bac038689073da9ed
yanliu1111 <liuyan0504@gmail.com>
2022-11-29 01:31:06 -0700

    logout done

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,18 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import SaveCoin from "../components/SaveCoin";
+import { useAuth } from "../context/AuthContext";
 
 const Account = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <div className="max-w-[1140px] mx-auto">
       <div className="flex justify-between items-center my-12 py-8 rounded-div">
         <div>
           <h1 className="text-2xl font-bold">Account</h1>
           <div>
-            <p>Welcome, User</p>
+            <p>Welcome, {user?.email}</p>
           </div>
         </div>
         <div>
-          <button className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">
+          <button
+            onClick={handleLogout}
+            className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
+          >
             Sign Out
           </button>
         </div>
