fe8dae49ecc1f9d8fd0213a4600a4a7c6adb9b8f
yanliu1111 <liuyan0504@gmail.com>
2022-11-29 01:18:23 -0700

    signup and connect firestore

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,17 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineMail, AiFillLock } from "react-icons/ai";
+import { useAuth } from "../context/AuthContext";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { signUp } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signUp(email, password);
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
+
   return (
     <div>
       <div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
         <h1 className="text-2xl font-bold">Sign Up</h1>
-        <form>
+        {error ? <p className="bg-red-30 p-3 my-2">{error}</p> : null}
+        <form onSubmit={handleSubmit}>
           <div className="my-4">
             <label>Email</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-full p-2 bg-primary border border-input rounded-2xl"
                 type="email"
               />
@@ -22,6 +42,7 @@ const Signup = () => {
             <label>Password</label>
             <div className="my-2 w-full relative rounded-2xl shadow-xl">
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 className="w-full p-2 bg-primary border border-input rounded-2xl"
                 type="password"
               />
