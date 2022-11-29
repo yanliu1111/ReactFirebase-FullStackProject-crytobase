9b60959d85e3c51417b499b26f625d45d429f8a5
yanliu1111 <liuyan0504@gmail.com>
2022-11-28 21:03:12 -0700

    SaveCoin page done

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import SaveCoin from "../components/SaveCoin";
 
 const Account = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Account</h1>
+          <p>Welcome, User</p>
+          <button>Sign Out</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>Watch List</h1>
+          <SaveCoin />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
