170d57ae7ece757365986e6e527f654fc2c9ef36
yanliu1111 <liuyan0504@gmail.com>
2022-11-28 21:23:47 -0700

    account page done

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -3,17 +3,24 @@ import SaveCoin from "../components/SaveCoin";
 
 const Account = () => {
   return (
-    <div>
-      <div>
+    <div className="max-w-[1140px] mx-auto">
+      <div className="flex justify-between items-center my-12 py-8 rounded-div">
         <div>
-          <h1>Account</h1>
-          <p>Welcome, User</p>
-          <button>Sign Out</button>
+          <h1 className="text-2xl font-bold">Account</h1>
+          <div>
+            <p>Welcome, User</p>
+          </div>
+        </div>
+        <div>
+          <button className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">
+            Sign Out
+          </button>
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Watch List</h1>
+      {/* between watch list and Acount page header*/}
+      <div className="flex justfiy-between items-center my-12 py-8 rounded-div">
+        <div className="w-full min-h-[300px]">
+          <h1 className="text-2xl font-bold py-4">Watch List</h1>
           <SaveCoin />
         </div>
       </div>
